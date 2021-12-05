d3b7e3199c3fcf8e4a0849c98974457bb82cbd86
Luciano Mendonca <luciano.s.mend@gmail.com>
2021-12-05 14:57:49 -0300

    Inclusão do health check

--- src/test/java/br/devops/tasks/functional/TasksTest.java
+++ src/test/java/br/devops/tasks/functional/TasksTest.java
@@ -16,7 +16,7 @@ public class TasksTest {
 	private WebDriver acessarAplicacao() throws MalformedURLException {
 //		WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.17:4444/wd/hub"), cap);
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.10:4444/wd/hub"), cap);
 		driver.navigate().to("http://192.168.0.17:8001/tasks/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
